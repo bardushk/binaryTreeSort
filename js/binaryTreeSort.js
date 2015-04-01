2c2513746035d8cf2077ed8d6eb440318a903311
bardushk <bardushk@gmail.com>
2015-04-01 05:38:04 +0300

    Class BinaryTree

--- js/binaryTreeSort.js
+++ js/binaryTreeSort.js
@@ -1,31 +1,52 @@
 var array = [10, 2, 4, 24, 15]; 
 
 $(document).ready(function(){
-
-
-alert('aha');
-
+	var tree = new BinaryTree(array);
+	tree.render();
 });
 
 // Класс узла дерева
 // 
 function Node(root){
-   this.root = root;
-   this.addNode = function(value){
-      if(value < root){
-         if(!this.leftBranch){
-            this.leftBranch = new Node(value);
-         }
-         else{
-            this.leftBranch.addNode(value);
-         }
-      }else{
-         if(!this.rightBranch){
-            this.rightBranch = new Node(value);
-         }
-         else{
-            this.rightBranch.addNode(value);
-         }
-      }
-   }
+	this.root = root;
+	// Добавляет узел
+	this.addNode = function(value){
+		if(value < root){
+			if(!this.leftBranch){
+				this.leftBranch = new Node(value);
+			}
+			else{
+				this.leftBranch.addNode(value);
+			}
+		}else{
+			if(!this.rightBranch){
+				this.rightBranch = new Node(value);
+			}
+			else{
+				this.rightBranch.addNode(value);
+			}
+		}
+	}
+	var that = this;
+	// Отображает узел
+	this.render = function(){
+		if(that.leftBranch){
+			that.leftBranch.render();
+		}
+		console.log(that.root);
+		if(that.rightBranch){
+			that.rightBranch.render();
+		}
+	}
+}
+
+function BinaryTree(array){
+	if(!$.isArray(array)) return;
+	var tree = new Node(array[0]);
+	for(var index = 1; index < array.length; index++ ){
+		tree.addNode(array[index]);
+	}
+	this.render = function(){
+		tree.render();
+	}
 }
